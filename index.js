5fc63d8b6f1a75cd3a8598cd1232124d8466e388
Quinn-Hub <56490493+Quinn-Hub@users.noreply.github.com>
2024-03-03 15:53:54 -0800

    Included in Index.js

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ const audioControlsScript = document.createElement("script");
 audioControlsScript.src = "./scripts/audio-controls.js";
 document.body.appendChild(audioControlsScript);
 
+const fullscreenScript = document.createElement("script");
+fullscreenScript.src = "./scripts/fullscreen.js";
+document.body.appendChild(fullscreenScript);
+
 // Load jQuery library
 const jqueryScript = document.createElement("script");
 jqueryScript.src = "https://code.jquery.com/jquery-3.6.3.min.js";
