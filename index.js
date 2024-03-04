a50649c09cc96909a85b10bbffa66d0bde42c3b3
timpt0261 <rchavez0215@gmail.com>
2024-03-03 16:33:46 -0800

    Added P5 Canvas

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 // index.js
 
-// Creating audio visualizer container
-const visualizerContainer = document.createElement("div");
-visualizerContainer.className = "canvas-container";
-
-// Creating label for the audio visualizer
-const label = document.createElement("label");
-label.innerText = "Audio Canvas";
-label.htmlFor = "audio-visualizer";
-label.className = "label";
-
-// Creating canvas for the audio visualizer
-const audioVisualizerCanvas = document.createElement("canvas");
-audioVisualizerCanvas.className = "canvas";
-audioVisualizerCanvas.id = "audio-visualizer";
-
-const ctx = audioVisualizerCanvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.strokeText("Audio Canvas", 10, 30);
-
-visualizerContainer.appendChild(label);
-visualizerContainer.appendChild(audioVisualizerCanvas);
-document.body.appendChild(visualizerContainer);
 
 //p5.js library
 const p5Script = document.createElement("script");
@@ -32,6 +10,13 @@ const html2CanvasScript = document.createElement("script");
 html2CanvasScript.src = "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.3/html2canvas.min.js";
 document.body.appendChild(html2CanvasScript);
 //js scripts
+const sketch = document.createElement("script");
+sketch.type = "text/javascript";
+sketch.src = "scripts/sketch.js";
+sketch.defer = true;
+
+document.body.appendChild(sketch);
+
 const audioControlsScript = document.createElement("script");
 audioControlsScript.src = "./scripts/audio-controls.js";
 document.body.appendChild(audioControlsScript);
@@ -45,8 +30,7 @@ const jqueryScript = document.createElement("script");
 jqueryScript.src = "https://code.jquery.com/jquery-3.6.3.min.js";
 document.body.appendChild(jqueryScript);
 
-/*   Lukas's Visualizer
-const audioVisualizerScript = document.createElement("script");
-audioVisualizerScript.src = "audiovisualizer1.js";
-document.body.appendChild(audioVisualizerScript);
-*/
+const canvas_container = document.createElement("div");
+canvas_container.id = "canvas-container";
+
+document.body.appendChild(canvas_container);
---
+++ scripts/sketch.js
@@ -0,0 +1,21 @@
+let canvasContainer;
+
+function setup() {
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  canvas.parent("canvas-container");
+  // resize canvas is the page is resized
+  $(window).resize(function () {
+    console.log("Resizing...");
+    resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  });
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
